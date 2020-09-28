5ba90436046adff064cd804767b0000967b84cf7
jay311311 <jay311311@gmail.com>
2020-09-28 20:41:03 +0900

    use state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Conponent, useState} from 'react'
+import List from"./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = ()=>{
+  const [todos, setTodos] = useState(["js공부"])
+  const [newTodo, setNewTodo] = useState()
+
+  const changeInputData=(event)=>{
+    setNewTodo(event.target.value)
+    //newTodo에 event.target.value 값을 넣음
+  }
+ 
+  const addTodo =(event)=>{
+    event.preventDefault()
+    setTodos([...todos, newTodo])
+//원래 있던 배열(todos)에 새로 받은 값(newTodo)을 넣음
+  }
+  return(
+    <>
+    <h1>todo application</h1>
+
+    <form action="" >
+      <input type="text" name=""  onChange={changeInputData}/>
+      <button onClick={addTodo}>할 일 추가</button>
+    </form>
+
+    <List todos={todos} />
+    </>
+  )
 }
 
-export default App;
+export default App
